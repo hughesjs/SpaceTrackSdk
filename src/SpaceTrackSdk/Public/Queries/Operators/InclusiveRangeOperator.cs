579daea5bc1603bbc22a3449e7afc62a9c3922ce
James Hughes <james@pyrosoftsolutions.co.uk>
2023-01-03 00:18:05 +0000

    refactor: Simplified type signature of InclusiveRangeOperator
    Signed-off-by: James Hughes <james@pyrosoftsolutions.co.uk>

--- src/SpaceTrackSdk/Public/Queries/Operators/InclusiveRangeOperator.cs
+++ src/SpaceTrackSdk/Public/Queries/Operators/InclusiveRangeOperator.cs
@@ -1,10 +1,15 @@
 namespace SpaceTrackSdk.Public.Queries.Operators;
 
-public class InclusiveRangeOperator<T>: Operator<(T From,T To)>
+public class InclusiveRangeOperator<T>: Operator<T>
 {
-	public InclusiveRangeOperator(T from, T to) : base((from, to)) { }
+	private readonly T _to;
+
+	public InclusiveRangeOperator(T from, T to) : base(from)
+	{
+		_to = to;
+	}
 
 	protected override string Opcode => "--";
 
-	public override string GetQueryString() => $"{Value.From}--{Value.To}";
+	public override string GetQueryString() => $"{Value}--{_to}";
 }

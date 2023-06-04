d70b3e51204b4f53c548807074c4e9fb05dd4a26
James Hughes <james@pyrosoftsolutions.co.uk>
2023-06-04 17:19:16 +0100

    Adjusted to account for possibility of no or low CDMs or Announcements
    Signed-off-by: James Hughes <james@pyrosoftsolutions.co.uk>

--- src/SpaceTrackSdk/Internal/Clients/ClientAdapter.cs
+++ src/SpaceTrackSdk/Internal/Clients/ClientAdapter.cs
@@ -17,7 +17,7 @@ internal class HttpClientAdapter<T>: IClientAdapter<T>
 	public async Task<T?> Get(string predicates)
 	{
 		T[]? arrayOfOne = await _client.GetFromJsonAsync<T[]>($"{_endpoint}/limit/1/{predicates}");
-		return arrayOfOne is null ? default : arrayOfOne.Single();
+		return arrayOfOne is null ? default : arrayOfOne.SingleOrDefault();
 	}
 	
 	public async Task<List<T>?> GetMany(string predicates) => await _client.GetFromJsonAsync<List<T>>($"{_endpoint}/{predicates}");
